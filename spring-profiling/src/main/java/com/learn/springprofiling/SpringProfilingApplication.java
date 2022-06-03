bd926e07665dca92694ec8f0d098279da7d2af84
harsh-shiyani <harsh.s@7span.com>
2022-06-03 15:48:33 +0530

    Initial Commit

---
+++ spring-profiling/src/main/java/com/learn/springprofiling/SpringProfilingApplication.java
@@ -0,0 +1,11 @@
+package com.learn.springprofiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfilingApplication{
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProfilingApplication.class, args);
+	}
+}
