884e4562973a8faed152740d4b75f5820b9dd1aa
harsh-shiyani <harsh.s@7span.com>
2022-06-03 16:00:47 +0530

    create and activate different profiles

--- spring-profiling/src/main/java/com/learn/springprofiling/SpringProfilingApplication.java
+++ spring-profiling/src/main/java/com/learn/springprofiling/SpringProfilingApplication.java
@@ -1,11 +1,28 @@
 package com.learn.springprofiling;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringProfilingApplication{
+public class SpringProfilingApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProfilingApplication.class, args);
+	}
+
+	@Value("${my.designation}")
+	private String designation;
+
+	@Value("${my.profile}")
+	private String profile;
+
+	@Value("${my.onlyOne}")
+	private String onlyOne;
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Designation : " + designation);
+		System.out.println("Profile : " + profile);
+		System.out.println("Not replaced by other profile : " + onlyOne);
 	}
 }
